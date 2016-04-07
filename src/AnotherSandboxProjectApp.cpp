e23f1cd67b72efe120f82d02edc6d458cf29fda5
Ilya Solovyov <astronaut.str@gmail.com>
2016-04-07 20:41:09 +0300

    Unsafe buffer implemented to optimize memory copying. WavetableOsc GLSL impl

--- src/AnotherSandboxProjectApp.cpp
+++ src/AnotherSandboxProjectApp.cpp
@@ -10,16 +10,214 @@ using namespace ci::app;
 using namespace ci::audio;
 using namespace std;
 
-#define LOGENABLED 1
+#define LOGENABLED 0
+#define UNSAFEBUFFER 1
 
 typedef GLfloat                     SampleValueType;
+
+template <typename T> class UnsafeRingBufferT
+{
+public:
+    //! Constructs a UnsafeRingBufferT with size = 0
+    UnsafeRingBufferT() : mData( nullptr ), mAllocatedSize( 0 ), mWriteIndex( 0 ), mReadIndex( 0 ) {}
+    //! Constructs a UnsafeRingBufferT with \a count maximum elements.
+    UnsafeRingBufferT( size_t count ) : mAllocatedSize( 0 )
+    {
+        resize( count );
+    }
+    
+    UnsafeRingBufferT( UnsafeRingBufferT &&other )
+    : mData( other.mData ), mAllocatedSize( other.mAllocatedSize ), mWriteIndex( 0 ), mReadIndex( 0 )
+    {
+        other.mData = nullptr;
+        other.mAllocatedSize = 0;
+    }
+    
+    ~UnsafeRingBufferT()
+    {
+        if( mData )
+            free( mData );
+    }
+    //! Resizes the container to contain \a count maximum elements. Invalidates the internal buffer and resets read / write indices to 0. \note Must be synchronized with both read and write threads.
+    void resize( size_t count )
+    {
+        size_t allocatedSize = count + 1; // one bin is used to distinguish between the read and write indices when full.
+        
+        if( mAllocatedSize )
+            mData = (T *)::realloc( mData, allocatedSize * sizeof( T ) );
+        else
+            mData = (T *)::calloc( allocatedSize, sizeof( T ) );
+        
+        CI_ASSERT( mData );
+        
+        mAllocatedSize = allocatedSize;
+        clear();
+    }
+    //! Invalidates the internal buffer and resets read / write indices to 0. \note Must be synchronized with both read and write threads.
+    void clear()
+    {
+        mWriteIndex = 0;
+        mReadIndex = 0;
+    }
+    //! Returns the maximum number of elements.
+    size_t getSize() const
+    {
+        return mAllocatedSize - 1;
+    }
+    //! Returns the number of elements available for wrtiing. \note Only safe to call from the write thread.
+    size_t getAvailableWrite() const
+    {
+        return getAvailableWrite( mWriteIndex, mReadIndex );
+    }
+    //! Returns the number of elements available for wrtiing. \note Only safe to call from the read thread.
+    size_t getAvailableRead() const
+    {
+        return getAvailableRead( mWriteIndex, mReadIndex );
+    }
+    
+    //! \brief Writes \a count elements into the internal buffer from \a array. \return `true` if all elements were successfully written, or `false` otherwise.
+    //!
+    //! \note only safe to call from the write thread.
+    //! TODO: consider renaming this to writeAll / readAll, and having generic read / write that just does as much as it can
+    bool write( const T *array, size_t count )
+    {
+        const size_t writeIndex = mWriteIndex.load( std::memory_order_relaxed );
+        const size_t readIndex = mReadIndex.load( std::memory_order_acquire );
+        
+        if( count > getAvailableWrite( writeIndex, readIndex ) )
+            return false;
+        
+        size_t writeIndexAfter = writeIndex + count;
+        
+        if( writeIndex + count > mAllocatedSize ) {
+            size_t countA = mAllocatedSize - writeIndex;
+            size_t countB = count - countA;
+            
+            std::memcpy( mData + writeIndex, array, countA * sizeof( T ) );
+            std::memcpy( mData, array + countA, countB * sizeof( T ) );
+            writeIndexAfter -= mAllocatedSize;
+        }
+        else {
+            std::memcpy( mData + writeIndex, array, count * sizeof( T ) );
+            if( writeIndexAfter == mAllocatedSize )
+                writeIndexAfter = 0;
+        }
+        
+        mWriteIndex.store( writeIndexAfter, std::memory_order_release );
+        return true;
+    }
+    //! \brief Reads \a count elements from the internal buffer into \a array.  \return `true` if all elements were successfully read, or `false` otherwise.
+    //!
+    //! \note only safe to call from the read thread.
+    bool read( T *array, size_t count )
+    {
+        const size_t writeIndex = mWriteIndex.load( std::memory_order_acquire );
+        const size_t readIndex = mReadIndex.load( std::memory_order_relaxed );
+        
+        if( count > getAvailableRead( writeIndex, readIndex ) )
+            return false;
+        
+        size_t readIndexAfter = readIndex + count;
+        
+        if( readIndex + count > mAllocatedSize ) {
+            size_t countA = mAllocatedSize - readIndex;
+            size_t countB = count - countA;
+            
+            std::memcpy( array, mData + readIndex, countA * sizeof( T ) );
+            std::memcpy( array + countA, mData, countB * sizeof( T ) );
+            
+            readIndexAfter -= mAllocatedSize;
+        }
+        else {
+            std::memcpy( array, mData + readIndex, count * sizeof( T ) );
+            if( readIndexAfter == mAllocatedSize )
+                readIndexAfter = 0;
+        }
+        
+        mReadIndex.store( readIndexAfter, std::memory_order_release );
+        return true;
+    }
+    
+    void getUnsafeDataWritePointer(size_t count, T*& firstPart, T*& secondPart, size_t& firstLength, size_t& secondLength)
+    {
+        firstPart = nullptr;
+        secondPart = nullptr;
+        firstLength = 0;
+        secondLength = 0;
+        
+        const size_t writeIndex = mWriteIndex.load( std::memory_order_relaxed );
+        const size_t readIndex = mReadIndex.load( std::memory_order_acquire );
+        
+        if( count > getAvailableWrite( writeIndex, readIndex ) )
+        {
+            return;
+        }
+        
+        size_t writeIndexAfter = writeIndex + count;
+        
+        if( writeIndex + count > mAllocatedSize )
+        {
+            size_t countA = mAllocatedSize - writeIndex;
+            size_t countB = count - countA;
+            
+            firstPart = mData + writeIndex;
+            firstLength = countA;
+            secondPart = mData;
+            secondLength = countB;
+            
+            writeIndexAfter -= mAllocatedSize;
+        }
+        else
+        {
+            firstPart = mData + writeIndex;
+            firstLength = count;
+            
+            if( writeIndexAfter == mAllocatedSize )
+                writeIndexAfter = 0;
+        }
+        
+        mWriteIndex.store( writeIndexAfter, std::memory_order_release );
+    }
+    
+private:
+    size_t getAvailableWrite( size_t writeIndex, size_t readIndex ) const
+    {
+        size_t result = readIndex - writeIndex - 1;
+        if( writeIndex >= readIndex )
+            result += mAllocatedSize;
+        
+        return result;
+    }
+    
+    size_t getAvailableRead( size_t writeIndex, size_t readIndex ) const
+    {
+        if( writeIndex >= readIndex )
+            return writeIndex - readIndex;
+        
+        return writeIndex + mAllocatedSize - readIndex;
+    }
+    
+    
+    T						*mData;
+    size_t					mAllocatedSize;
+    std::atomic<size_t>		mWriteIndex, mReadIndex;
+};
+
+
+#if UNSAFEBUFFER
+typedef UnsafeRingBufferT<SampleValueType> RingBuffer;
+#else
+typedef ci::audio::dsp::RingBufferT<SampleValueType> RingBuffer;
+#endif
+
 
 class ExternalDSPNode : public GenNode
 {
 protected:
-    ci::audio::dsp::RingBufferT<SampleValueType>* _ringBuffer;
+    RingBuffer* _ringBuffer;
+
 public:
-    ExternalDSPNode(ci::audio::dsp::RingBufferT<SampleValueType>* externalRingBuffer)
+    ExternalDSPNode(RingBuffer* externalRingBuffer)
     {
         _ringBuffer = externalRingBuffer;
     }
@@ -49,7 +247,7 @@ typedef std::shared_ptr<class ExternalDSPNode> ExternalDSPNodeRef;
 class GPUDSPController
 {
 public:
-    ci::audio::dsp::RingBufferT<SampleValueType> RingBuffer;
+    RingBuffer RingBuffer;
     
 private:
     GLuint _nodesVBO;
@@ -246,10 +444,29 @@ public:
         glUseProgram(0);
         
         // recieve processed data from feedback
-        glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, toWrite * sizeof(SampleValueType), _feedbackData);
+#if UNSAFEBUFFER
+        SampleValueType* firstPart = nullptr;
+        SampleValueType* secondPart = nullptr;
+        size_t firstLength = 0;
+        size_t secondLength = 0;
         
+        RingBuffer.getUnsafeDataWritePointer(toWrite, firstPart, secondPart, firstLength, secondLength);
+        if (firstPart != nullptr)
+        {
+            glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, firstLength * sizeof(SampleValueType), firstPart);
+            _samplesProcessed += firstLength;
+            if (secondPart != nullptr)
+            {
+                glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, firstLength * sizeof(SampleValueType), secondLength * sizeof(SampleValueType), secondPart);
+                _samplesProcessed += secondLength;
+            }
+            
+        }
+#else
+        glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, toWrite * sizeof(SampleValueType), _feedbackData);
         RingBuffer.write(_feedbackData, toWrite);
         _samplesProcessed += toWrite;
+#endif
         
 #if LOGENABLED
         std::cerr << "[ProcessingThread]: processed " << toWrite << "samples" << std::endl;
@@ -289,7 +506,7 @@ void AnotherSandboxProjectApp::setup()
     const size_t bufferSize = outputNode->getFramesPerBlock();
     
     const size_t audioBuffersInGPUBuffer = 4;
-    const size_t GPUBuffersInRingBuffer = 4;
+    const size_t GPUBuffersInRingBuffer = 2;
     _DSPController = new GPUDSPController(sampleRate, bufferSize * audioBuffersInGPUBuffer * GPUBuffersInRingBuffer);
     
     externalDSPNode = ctx->makeNode(new ExternalDSPNode(&_DSPController->RingBuffer));
--- src/GPUDSP.vert
+++ src/GPUDSP.vert
@@ -5,7 +5,7 @@
 #define M_PI_4      0.78539816339744830961566084581987572
 #define M_2PI       (2.0 * M_PI)
 
-#define TEST_FREQUENCY 60.0
+#define TEST_FREQUENCY 73.3
 
 in int samplePosition;
 
@@ -22,16 +22,47 @@ float testSineWave()
     return sin(M_2PI * time * TEST_FREQUENCY);
 }
 
-float testWaveTable()
+float testWaveTableInt()
 {
-    float time = float(samplesProcessed + samplePosition) / float(sampleRate);
-    int tableIndex = int(fract(time * TEST_FREQUENCY) * float(sampleRate));
+    int fInt = int(TEST_FREQUENCY);
+    float fFract = fract(TEST_FREQUENCY);
+    
+    int spmsr = samplesProcessed % sampleRate;
+    int fimsr = fInt % sampleRate;
+    int simsr = samplePosition % sampleRate;
+    
+    int aPart = (spmsr * fimsr) % sampleRate + (simsr * fimsr) % sampleRate;
+    int bPart = int(float(samplesProcessed) * fFract) % sampleRate + int(float(samplePosition) * fFract) % sampleRate;
+    
+    int tableIndex = (aPart + bPart) % sampleRate;
     
     return texelFetch(waveTable, tableIndex).x;
 }
 
+float testWaveTable()
+{
+    float time = float(samplesProcessed + samplePosition) / float(sampleRate);
+    int tableIndex = int(fract(time * TEST_FREQUENCY) * sampleRate + 0.5);
+    
+    return texelFetch(waveTable, tableIndex).x;
+}
+
+float waveTableOsc(float frequency)
+{
+    int     frecInt     = int(frequency);
+    float   frecFract   = fract(frequency);
+    int     SPmSR       = samplesProcessed % sampleRate;
+    int     FImSR       = frecInt % sampleRate;
+    int     SImSR       = samplePosition % sampleRate;
+    int     intPart     = (SPmSR * FImSR) % sampleRate + (SImSR * FImSR) % sampleRate;
+    int     fractPart   = int(float(samplesProcessed) * frecFract) % sampleRate + int(float(samplePosition) * frecFract) % sampleRate;
+    
+    int     tableIndex  = (intPart + fractPart) % sampleRate;
+    
+    return  texelFetch(waveTable, tableIndex).x;
+}
+
 void main()
 {
-    //sampleValue = testSineWave();
-    sampleValue = testWaveTable();
+    sampleValue = waveTableOsc(TEST_FREQUENCY);
 }
